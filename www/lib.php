7a64f716984f94e807b5a7d275c76b5ab9de90b8
오준영 <icon1123@gmail.com>
2016-08-10 10:27:45 +0900

    1

---
+++ www/lib.php
@@ -0,0 +1,62 @@
+<?php
+
+function echoJsInPath($path)
+{
+    $realPath = $_SERVER['DOCUMENT_ROOT'] . $path;
+    
+    if (! is_dir($realPath)) {
+        return;
+    }
+    
+    $h = opendir($realPath);
+    
+    while (false !== ($file = readdir($h))) {
+        if ($file == '.' || $file == '..') {
+            continue;
+        }
+        
+        if (is_dir($realPath . $file)) {
+            echoJsInPath($path . $file . '/');
+            
+            continue;
+        }
+        
+        if (strpos($file, '.js') === false)
+            continue;
+        
+        echo '<script src="' . $path . $file . '"></script>
+';
+    }
+}
+
+function getBodyClass($classString)
+{
+    global $pagePath;
+    
+    $namePartList = explode("-", str_replace(".php", "", basename($pagePath)));
+    $pageClaseList = [];
+    
+    for ($i = 0; $i < count($namePartList); $i ++) {
+        $arr = [];
+        $arr[] = "page";
+        
+        for ($j = 0; $j <= $i; $j ++) {
+            $arr[] = $namePartList[$j];
+        }
+        
+        $pageClaseList[] = implode("-", $arr);
+    }
+    
+    $pageClassList2 = explode(" ", $classString);
+    
+    return implode(" ", array_unique(array_merge($pageClaseList, $pageClassList2)));
+}
+
+function getDepthId($depth)
+{
+    global $pagePath;
+    
+    $namePartList = explode("-", str_replace(".php", "", basename($pagePath)));
+    
+    return isset($namePartList[$depth]) ? $namePartList[$depth] : null;
+}
